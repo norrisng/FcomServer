21590857b15ed5ecc5212690cc6b98c13c6653ec
Norris Ng <6787487+norrisng@users.noreply.github.com>
2019-03-27 19:22:37 -0700

    Remove SQLite DB initialization code

--- main_api.py
+++ main_api.py
@@ -1,7 +1,4 @@
 from api.message_api import app
-from schema import init_messages_db, init_registration_db
 
 if __name__ == "__main__":
-    init_registration_db()
-    init_messages_db()
     app.run(host='0.0.0.0', debug=False)
